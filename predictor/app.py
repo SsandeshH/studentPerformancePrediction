2be6a64189d55ef46ec192b352e98f6bc00cbb2e
San <ssandeshupretii@gmail.com>
2025-05-19 01:05:24 +0545

    Made functional app, all 3 models implemented, added pre-commit hooks

--- predictor/app.py
+++ predictor/app.py
@@ -1,10 +1,59 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from enum import Enum
+import joblib
+import os
+import pandas as pd
 
 app = FastAPI()
 
+
+class input_datatype(
+    BaseModel
+):  # The Type of Data that user should enter. BaseModel is a pydantic model for dat validation
+    Hours_Studied: int
+    Attendance: int
+    Extracurricular_Activities: str
+    Sleep_Hours: int
+    Previous_Scores: float
+
+
+class ModelName(str, Enum):  # Not a pydantic model's instance
+    linear = "LinearRegression"
+    Lasso = "Lasso"
+    Ridge = "Ridge"
+
+
 @app.get("/")
-async def root():   #async makes root() a coroutine, which can we stopped and presumed, dont wait for one task to complete
+async def root():  # async makes root() a coroutine, which can we stopped and presumed, dont wait for one task to complete
     return {"Welcome to Student Performance Prediction App"}
 
 
+@app.post("/predict")
+async def prediction(data: input_datatype, model: ModelName):
+    if model.value == "LinearRegression":
+        model_selected = os.getenv("LINEAR_MODEL_PATH")
 
+    elif model.value == "Lasso":
+        model_selected = os.getenv("LASSO_MODEL_PATH")
+
+    else:
+        model_selected = os.getenv("RIDGE_MODEL_PATH")
+
+    if not os.path.exists(model_selected):
+        raise HTTPException(status_code=500, detail=f"Model not found at {model_selected}")
+
+    model_pipeline = joblib.load(model_selected)
+
+    sample = pd.DataFrame([data])
+
+    try:
+        prediction = model_pipeline.predict(sample)[0]
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {
+        "model_used": model.value,
+        "predicted_score": round(float(prediction), 2),
+        "input": data,
+    }
